9b1627a12899539cce64b9704a710e00e1f6560b
grmkris <kristjan.grm1@gmail.com>
2024-06-01 16:31:29 +0200

    new stuff

---
+++ apps/contracts-foundry/src/TokenSubmisson.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+
+
+contract MyERC20 is ERC20 {
+    address public owner;
+
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        owner = msg.sender;
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == owner, "Not authorized");
+        _mint(to, amount);
+    }
+}
+
+contract TokenSubmission {
+    struct TokenIdea {
+        string name;
+        string symbol;
+        uint256 votes;
+    }
+
+    address public owner;
+    uint256 public targetAmount;
+    uint256 public totalCollected;
+    TokenIdea[] public tokenIdeas;
+    mapping(address => bool) public hasSubmitted;
+
+    IPoolManager public uniPoolManager;
+    address public WETH;
+
+    constructor(uint256 _targetAmount, address _uniPoolManager, address _WETH) {
+        owner = msg.sender;
+        targetAmount = _targetAmount;
+        uniPoolManager = IPoolManager(_uniPoolManager);
+        WETH = _WETH;
+    }
+
+    function submitTokenIdea(string calldata _name, string calldata _symbol) external {
+        require(!hasSubmitted[msg.sender], "Already submitted");
+        tokenIdeas.push(TokenIdea({name: _name, symbol: _symbol, votes: 0}));
+        hasSubmitted[msg.sender] = true;
+    }
+
+    function contributeAndVote(uint256 _ideaIndex) external payable {
+        require(_ideaIndex < tokenIdeas.length, "Invalid index");
+        require(msg.value > 0, "Must contribute Ether");
+
+        totalCollected += msg.value;
+        tokenIdeas[_ideaIndex].votes += msg.value;
+    }
+
+    function createTokenAndAddLiquidity() external {
+        require(totalCollected >= targetAmount, "Target not met");
+
+        TokenIdea memory winningIdea = getWinningIdea();
+        MyERC20 newToken = new MyERC20(winningIdea.name, winningIdea.symbol);
+
+        uint256 liquidityAmount = totalCollected / 2;
+        uint256 tokenAmount = liquidityAmount * 1000; // Example ratio
+
+        newToken.mint(address(this), tokenAmount);
+
+        newToken.approve(address(uniPoolManager), tokenAmount);
+        // TODO create pool with univ4 and add liquidity
+
+        payable(owner).transfer(address(this).balance);
+    }
+
+    function getWinningIdea() internal view returns (TokenIdea memory) {
+        TokenIdea memory winningIdea;
+        uint256 maxVotes = 0;
+
+        for (uint256 i = 0; i < tokenIdeas.length; i++) {
+            if (tokenIdeas[i].votes > maxVotes) {
+                winningIdea = tokenIdeas[i];
+                maxVotes = tokenIdeas[i].votes;
+            }
+        }
+
+        return winningIdea;
+    }
+}
